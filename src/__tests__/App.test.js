ed1182846646b26a941b6542f0c375c095359ad4
danielkamweru <daniel.kamweru@student.moringaschool.com>
2025-10-21 12:25:52 +0300

    events codealong

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 import App from "../App";
 
-import '@testing-library/jest-dom';
+test("selected topping disappears when checked a second time", () => {
+  render(<App />);
 
-// Code tests here
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+  userEvent.click(addPepperoni);
+
+  expect(addPepperoni).toBeChecked();
+  expect(screen.getByText("Cheese")).toBeInTheDocument();
+  expect(screen.getByText("Pepperoni")).toBeInTheDocument();
+
+  userEvent.click(addPepperoni);
+
+  expect(addPepperoni).not.toBeChecked();
+  expect(screen.getByText("Cheese")).toBeInTheDocument();
+  expect(screen.queryByText("Pepperoni")).not.toBeInTheDocument();
+});
